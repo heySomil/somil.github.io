f33a2aea86e5b23b2c47377b1d46ad6c83e2dd8b
Somil Yadav <91374252+heySomil@users.noreply.github.com>
2023-07-20 17:20:27 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function menufunc() {
+    let menu = document.querySelector('#menu')
+    let nav = document.querySelector('.nav');
+    nav.classList.toggle('open');
+}
+function updateProject(){
+    alert("Projects will be showcased soon")
+}
